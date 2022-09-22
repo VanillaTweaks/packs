aaffa8aceb9c27bc474e3d438ff5a6ffc50b8d75
Grant Gryczan <grantgryczan@gmail.com>
2022-09-22 12:10:28 -0400

    Remove redundant class arguments

--- lib/datapacks/config/types.py
+++ lib/datapacks/config/types.py
@@ -9,50 +9,48 @@ class ConfigOptionBase(BaseModel, frozen=True):
     description: Any
 
 
-class ServerConfigOptionBase(ConfigOptionBase, frozen=True):
+class ServerConfigOptionBase(ConfigOptionBase):
     # Which target of the `pack.config` objective to store the option's value in.
     target: StrictStr
 
 
-class PlayerConfigOptionBase(ConfigOptionBase, frozen=True):
+class PlayerConfigOptionBase(ConfigOptionBase):
     # The objective that stores the option's value for each player, and stores the
     #  current server default value in `$default`.
     objective: StrictStr
 
 
-class BoolConfigOptionBase(ConfigOptionBase, frozen=True):
+class BoolConfigOptionBase(ConfigOptionBase):
     default: StrictBool
 
 
-class IntConfigOptionBase(ConfigOptionBase, frozen=True):
+class IntConfigOptionBase(ConfigOptionBase):
     # A string of the range of integers this option accepts (e.g. `"0..100"`, `"1.."`).
     accepts: StrictStr
     default: StrictInt
 
 
 @final
-class ConfigOptionChoice(ConfigOptionBase, frozen=True):
+class ConfigOptionChoice(ConfigOptionBase):
     default: StrictBool = False
 
 
-class ChoiceConfigOptionBase(ConfigOptionBase, frozen=True):
+class ChoiceConfigOptionBase(ConfigOptionBase):
     choices: list[ConfigOptionChoice]
 
 
 @final
-class ServerBoolConfigOption(BoolConfigOptionBase, ServerConfigOptionBase, frozen=True):
+class ServerBoolConfigOption(BoolConfigOptionBase, ServerConfigOptionBase):
     pass
 
 
 @final
-class ServerIntConfigOption(IntConfigOptionBase, ServerConfigOptionBase, frozen=True):
+class ServerIntConfigOption(IntConfigOptionBase, ServerConfigOptionBase):
     pass
 
 
 @final
-class ServerChoiceConfigOption(
-    ChoiceConfigOptionBase, ServerConfigOptionBase, frozen=True
-):
+class ServerChoiceConfigOption(ChoiceConfigOptionBase, ServerConfigOptionBase):
     pass
 
 
@@ -62,19 +60,17 @@ ServerConfigOption = (
 
 
 @final
-class PlayerBoolConfigOption(BoolConfigOptionBase, PlayerConfigOptionBase, frozen=True):
+class PlayerBoolConfigOption(BoolConfigOptionBase, PlayerConfigOptionBase):
     pass
 
 
 @final
-class PlayerIntConfigOption(IntConfigOptionBase, PlayerConfigOptionBase, frozen=True):
+class PlayerIntConfigOption(IntConfigOptionBase, PlayerConfigOptionBase):
     pass
 
 
 @final
-class PlayerChoiceConfigOption(
-    ChoiceConfigOptionBase, PlayerConfigOptionBase, frozen=True
-):
+class PlayerChoiceConfigOption(ChoiceConfigOptionBase, PlayerConfigOptionBase):
     pass
 
 
